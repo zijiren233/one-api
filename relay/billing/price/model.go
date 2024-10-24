86af260741d4f5dfb77efbf862604e6f6e7171ad
zijiren233 <pyh1670605849@gmail.com>
2024-10-24 04:01:01 +0000

    chore: claude-3-5-sonnet-20241022

--- relay/billing/price/model.go
+++ relay/billing/price/model.go
@@ -80,6 +80,7 @@ var ModelPrice = map[string]float64{
 	"claude-3-haiku-20240307":    0.00175,
 	"claude-3-sonnet-20240229":   0.021,
 	"claude-3-5-sonnet-20240620": 0.021,
+	"claude-3-5-sonnet-20241022": 0.021,
 	"claude-3-opus-20240229":     0.105,
 	// https://cloud.baidu.com/doc/WENXINWORKSHOP/s/hlrk4akp7
 	"ERNIE-4.0-8K":       0.12,
