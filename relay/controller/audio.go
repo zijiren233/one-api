580d42cda79ed5669d18e5d1d8de9e656fc097df
zijiren233 <pyh1670605849@gmail.com>
2024-10-29 07:51:16 +0000

    fix: audio modelmapping

--- relay/controller/audio.go
+++ relay/controller/audio.go
@@ -77,16 +77,9 @@ func RelayAudioHelper(c *gin.Context, relayMode int) *relaymodel.ErrorWithStatus
 		return openai.ErrorWrapper(errors.New("group balance is not enough"), "insufficient_group_balance", http.StatusForbidden)
 	}
 	// map model name
-	modelMapping := c.GetString(ctxkey.ModelMapping)
-	if modelMapping != "" {
-		modelMap := make(map[string]string)
-		err := json.Unmarshal([]byte(modelMapping), &modelMap)
-		if err != nil {
-			return openai.ErrorWrapper(err, "unmarshal_model_mapping_failed", http.StatusInternalServerError)
-		}
-		if modelMap[audioModel] != "" {
-			audioModel = modelMap[audioModel]
-		}
+	modelMapping := c.GetStringMapString(ctxkey.ModelMapping)
+	if modelMapping[audioModel] != "" {
+		audioModel = modelMapping[audioModel]
 	}
 
 	baseURL := channeltype.ChannelBaseURLs[channelType]
