8e0985b5cb3b9862c6a99826cc6fabe484a136a3
zijiren233 <pyh1670605849@gmail.com>
2024-11-13 08:35:43 +0000

    fix: doubao bot support

--- relay/adaptor/doubao/main.go
+++ relay/adaptor/doubao/main.go
@@ -2,6 +2,7 @@ package doubao
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/songquanpeng/one-api/relay/meta"
 	"github.com/songquanpeng/one-api/relay/relaymode"
@@ -10,6 +11,9 @@ import (
 func GetRequestURL(meta *meta.Meta) (string, error) {
 	switch meta.Mode {
 	case relaymode.ChatCompletions:
+		if strings.HasPrefix(meta.ActualModelName, "bot-") {
+			return fmt.Sprintf("%s/api/v3/bots/chat/completions", meta.BaseURL), nil
+		}
 		return fmt.Sprintf("%s/api/v3/chat/completions", meta.BaseURL), nil
 	case relaymode.Embeddings:
 		return fmt.Sprintf("%s/api/v3/embeddings", meta.BaseURL), nil
